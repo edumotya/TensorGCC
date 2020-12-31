2aa55513fe616029e7b44acc8224931af36e66cf
edu <motyaedu@gmail.com>
2020-12-31 11:04:51 +0100

    batch

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="tensorgcc",
-    version="0.1",
+    version="0.2",
     install_requires="tensorflow>=2",
     description="TensorFlow implementation of the generalized cross-correlation (GCC).",
     author="Eduardo Cuesta",
